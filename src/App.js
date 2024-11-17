3b1f7540f6ce3dc6ee08fca45442bbfb10fefef1
Sebastian Robi <76728638+seb-rob@users.noreply.github.com>
2024-11-17 06:48:28 +0000

    deploy to ec2 6

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
+          ReactJS on Action....
+        </p>
+        <p>
           NGINX UP AND RUNNING...
         </p>
         <a
